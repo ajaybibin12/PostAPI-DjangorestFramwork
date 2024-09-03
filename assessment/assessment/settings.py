794dde41b34f56badc1714e8808677c6fa65ef61
ajaybibin12 <ajaybibin12@gmail.com>
2024-09-04 02:37:28 +0530

    added cors headers

--- assessment/assessment/settings.py
+++ assessment/assessment/settings.py
@@ -48,7 +48,8 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework_simplejwt',
     'app',
-    'drf_yasg'
+    'drf_yasg',
+    'corsheaders',
 ]
 
 REST_FRAMEWORK = {
@@ -98,7 +99,16 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware'
 ]
+
+
+CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOWED_ORIGINS = [
+    "https://socialmedia-posts-api.onrender.com",
+]
+
+
 
 ROOT_URLCONF = 'assessment.urls'
 
