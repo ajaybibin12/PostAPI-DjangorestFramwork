7adb9ddf96d7ce61c32995274ad7fd09f3e2f9c1
ajaybibin12 <ajaybibin12@gmail.com>
2024-09-04 01:42:24 +0530

    changes in manage.py.

--- assessment/manage.py
+++ assessment/manage.py
@@ -1,8 +1,32 @@
+# #!/usr/bin/env python
+# """Django's command-line utility for administrative tasks."""
+# import os
+# import sys
+
+
+# def main():
+#     """Run administrative tasks."""
+#     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'assessment.settings')
+#     try:
+#         from django.core.management import execute_from_command_line
+#     except ImportError as exc:
+#         raise ImportError(
+#             "Couldn't import Django. Are you sure it's installed and "
+#             "available on your PYTHONPATH environment variable? Did you "
+#             "forget to activate a virtual environment?"
+#         ) from exc
+#     execute_from_command_line(sys.argv)
+
+
+# if __name__ == '__main__':
+#     main()
+
+
+
 #!/usr/bin/env python
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-
 
 def main():
     """Run administrative tasks."""
@@ -15,8 +39,15 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
-    execute_from_command_line(sys.argv)
+    
+    # If no port is specified, default to 8000
+    port = os.environ.get('PORT', '8000')
 
+    # Modify sys.argv directly to include the host and port
+    if 'runserver' in sys.argv:
+        sys.argv[sys.argv.index('runserver') + 1:] = [f'0.0.0.0:{port}']
+
+    execute_from_command_line(sys.argv)
 
 if __name__ == '__main__':
     main()
